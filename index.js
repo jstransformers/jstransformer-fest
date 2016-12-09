b5a0763c7f02a697998c8b93d37c0fd1d8fd3c6a
Rob Loach <robloach@gmail.com>
2016-12-09 13:27:45 -0500

    Add xo as coding style (#37)

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
-'use strict';
+'use strict'
 
-var fest = require('fest');
-exports.name = 'fest';
-exports.outputFormat = 'html';
+var fest = require('fest')
+
+exports.name = 'fest'
+exports.outputFormat = 'html'
 
 exports.compileFile = function (file, options) {
-  options = options || {};
-  var compiled = fest.compile(file, options);
-  return new Function ('return ' + compiled)();
+  options = options || {}
+  var compiled = fest.compile(file, options)
+  // eslint-disable-next-line no-new-func
+  return new Function('return ' + compiled)()
 }
